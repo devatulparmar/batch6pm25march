d53cf68d2c0e3bd4b241accf20f2d9bf58a88fb4
devatulparmar <dev.atulparmar@gmail.com>
2025-04-16 18:58:19 +0530

    Column and Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,147 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        title: const Text("Hello"),
-      ),
-      body: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(
-            "This is body part",
-            style: TextStyle(
-              fontSize: 36,
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 36,
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          print("Clicked");
-        },
-        child: Icon(
-          Icons.add,
+  runApp(
+    const MyApp(),
+  );
+}
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            "My First App",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.black,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    // color: Colors.red,
+                    height: 100,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15, top: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.black, width: 5),
+                    ),
+                    child: const Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // color: Colors.red,
+                    height: 100,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15, top: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.black, width: 5),
+                    ),
+                    child: const Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // color: Colors.red,
+                    height: 100,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15, top: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.black, width: 5),
+                    ),
+                    child: const Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    // color: Colors.red,
+                    height: 100,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15, top: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.brown,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.black, width: 5),
+                    ),
+                    child: const Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // color: Colors.red,
+                    height: 100,
+                    width: 100,
+                    padding: const EdgeInsets.only(left: 15, top: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.black, width: 5),
+                    ),
+                    child: const Text(
+                      "Hello",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
