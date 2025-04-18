7ca0f91a3313efc4c32a0484a80ad4a92efe6dcd
devatulparmar <dev.atulparmar@gmail.com>
2025-04-18 18:36:16 +0530

    SizedBox, leading, actions

--- lib/main.dart
+++ lib/main.dart
@@ -21,126 +21,155 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.brown,
           title: const Text(
             "My First App",
             style: TextStyle(
               color: Colors.white,
             ),
           ),
-        ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              color: Colors.black,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    // color: Colors.red,
-                    height: 100,
-                    width: 100,
-                    padding: const EdgeInsets.only(left: 15, top: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.black, width: 5),
-                    ),
-                    child: const Text(
-                      "Hello",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    // color: Colors.red,
-                    height: 100,
-                    width: 100,
-                    padding: const EdgeInsets.only(left: 15, top: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.black, width: 5),
-                    ),
-                    child: const Text(
-                      "Hello",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    // color: Colors.red,
-                    height: 100,
-                    width: 100,
-                    padding: const EdgeInsets.only(left: 15, top: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.black, width: 5),
-                    ),
-                    child: const Text(
-                      "Hello",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
+          actions: [
+            const Text(
+              "Click",
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
-            Container(
-              color: Colors.black,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    // color: Colors.red,
-                    height: 100,
-                    width: 100,
-                    padding: const EdgeInsets.only(left: 15, top: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.brown,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.black, width: 5),
-                    ),
-                    child: const Text(
-                      "Hello",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    // color: Colors.red,
-                    height: 100,
-                    width: 100,
-                    padding: const EdgeInsets.only(left: 15, top: 25),
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: Colors.black, width: 5),
-                    ),
-                    child: const Text(
-                      "Hello",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            const SizedBox(
+              width: 10,
             ),
           ],
+          elevation: 50,
+          leading: Container(
+            padding: const EdgeInsets.only(top: 15, left: 5),
+            child: const Text(
+              "Hello World",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          leadingWidth: 80,
+          centerTitle: true,
         ),
+        // body: Column(
+        //   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     Container(
+        //       color: Colors.black,
+        //       child: Row(
+        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //         children: [
+        //           Container(
+        //             // color: Colors.red,
+        //             height: 100,
+        //             width: 100,
+        //             padding: const EdgeInsets.only(left: 15, top: 25),
+        //             decoration: BoxDecoration(
+        //               color: Colors.red,
+        //               borderRadius: BorderRadius.circular(100),
+        //               border: Border.all(color: Colors.black, width: 5),
+        //             ),
+        //             child: const Text(
+        //               "Hello",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 18,
+        //               ),
+        //             ),
+        //           ),
+        //           Container(
+        //             // color: Colors.red,
+        //             height: 100,
+        //             width: 100,
+        //             padding: const EdgeInsets.only(left: 15, top: 25),
+        //             decoration: BoxDecoration(
+        //               color: Colors.blue,
+        //               borderRadius: BorderRadius.circular(100),
+        //               border: Border.all(color: Colors.black, width: 5),
+        //             ),
+        //             child: const Text(
+        //               "Hello",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 18,
+        //               ),
+        //             ),
+        //           ),
+        //           Container(
+        //             // color: Colors.red,
+        //             height: 100,
+        //             width: 100,
+        //             padding: const EdgeInsets.only(left: 15, top: 25),
+        //             decoration: BoxDecoration(
+        //               color: Colors.orange,
+        //               borderRadius: BorderRadius.circular(100),
+        //               border: Border.all(color: Colors.black, width: 5),
+        //             ),
+        //             child: const Text(
+        //               "Hello",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 18,
+        //               ),
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //     Container(
+        //       color: Colors.black,
+        //       child: Column(
+        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //         children: [
+        //           Container(
+        //             // color: Colors.red,
+        //             height: 100,
+        //             width: 100,
+        //             padding: const EdgeInsets.only(left: 15, top: 25),
+        //             decoration: BoxDecoration(
+        //               color: Colors.brown,
+        //               borderRadius: BorderRadius.circular(100),
+        //               border: Border.all(color: Colors.black, width: 5),
+        //             ),
+        //             child: const Text(
+        //               "Hello 1",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 18,
+        //               ),
+        //             ),
+        //           ),
+        //           const SizedBox(
+        //             height: 150,
+        //           ),
+        //           Container(
+        //             // color: Colors.red,
+        //             height: 100,
+        //             width: 100,
+        //             padding: const EdgeInsets.only(left: 15, top: 25),
+        //             decoration: BoxDecoration(
+        //               color: Colors.green,
+        //               borderRadius: BorderRadius.circular(100),
+        //               border: Border.all(color: Colors.black, width: 5),
+        //             ),
+        //             child: const Text(
+        //               "Hello 2",
+        //               style: TextStyle(
+        //                 color: Colors.white,
+        //                 fontSize: 18,
+        //               ),
+        //             ),
+        //           ),
+        //           const SizedBox(
+        //             height: 50,
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
